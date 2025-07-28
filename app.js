29813c7c7a7d22c3248d2b5583d0136e5d1ecad5
Farhan Akhtar <farhanakhter74786@gmail.com>
2025-07-28 20:32:39 +0530

    Code update

---
+++ app.js
@@ -0,0 +1,35 @@
+let url = "http://universities.hipolabs.com/search?name=india";
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", async () => {
+    let val = document.querySelector("input").value;
+    console.log(val);
+
+    let colleges = await getColleges();
+    console.log(colleges);
+})
+
+function show(colleges) {
+    let list = document.querySelector("#list")
+    list.innerText = "";
+
+    for(col of colleges) {
+        console.log(col[i].state-province);
+        let li = document.createElement("li")
+        li.innerText = col.name;
+        list.appendChild(li);
+    }
+}
+
+
+async function getColleges() {
+    try {
+        let res = await axios.get(url);
+        console.log(res.data);
+    } catch(err) {
+        console.log("error: ", e);
+        return [];
+    }
+}
+
+getColleges();
